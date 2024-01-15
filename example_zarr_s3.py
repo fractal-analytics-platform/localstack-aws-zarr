e09866565a9af185eb47e106748f4487acacf540
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2024-01-15 14:19:15 +0100

    Clean up example_zarr_s3.py

--- example_zarr_s3.py
+++ example_zarr_s3.py
@@ -1,4 +1,3 @@
-import sys
 import zarr
 import dask.array as da
 import os
@@ -12,52 +11,43 @@ os.environ["AWS_ENDPOINT_URL"] = "http://localhost:4566"
 
 
 # Define URL of root group
-group_url = f"s3://archive-bucket/my_group.zarr"
+group_url = "s3://archive-bucket/my_group.zarr"
+
+# Remove Zarr group
+print(f"Remove {group_url=}, if it exists")
+subprocess.run(
+    shlex.split(f"awslocal s3 rm {group_url} --recursive"),
+    check=True,
+    capture_output=True,
+)
 
 # Create group and subgroup
-print("Open group")
 root_group = zarr.open_group(group_url)
-print(f"{root_group=}")
-print()
-print("Create subgroup")
+print(f"Opened {root_group=}")
 subgroup = root_group.create_group("subgroup")
-print(f"{subgroup=}")
-print()
+subgroup_url = f"{group_url}/subgroup"
+print(f"Created {subgroup=}")
 
 # Write data to array in root group
 array_0_url = f"{group_url}/array_0"
 print(f"Write data to {group_url=}")
 data_0 = da.from_array([0, 0, 0, 0], chunks=(2,))
 data_0.to_zarr(array_0_url, storage_options={'anon': False})
-print()
 
 # Read array-0 data
 print(f"Read data from {array_0_url=}")
 actual_data = da.from_zarr(array_0_url, storage_options={'anon': False})
-print(f"{actual_data=}")
-print(f"{actual_data.compute()=}")
-print()
+print(f"  {actual_data=}")
+print(f"  {actual_data.compute()=}")
 
 # Write data to array in subgroup
-subgroup_url = f"{group_url}/subgroup"
 array_1_url = f"{subgroup_url}/array_1"
 print(f"Write data to {array_1_url=}")
 data_1 = da.from_array([1, 1, 1, 1], chunks=(2,))
 data_1.to_zarr(array_1_url, storage_options={'anon': False})
-print()
 
 # Read array-0 data
 print(f"Read data from {array_1_url=}")
 actual_data = da.from_zarr(array_1_url, storage_options={'anon': False})
-print(f"{actual_data=}")
-print(f"{actual_data.compute()=}")
-print()
-
-# Clean up
-print("Now remove the s3 object")
-subprocess.run(
-    shlex.split(
-        f"awslocal s3 rm {group_url} --recursive"
-    ),
-    check=True,
-)
+print(f"  {actual_data=}")
+print(f"  {actual_data.compute()=}")
